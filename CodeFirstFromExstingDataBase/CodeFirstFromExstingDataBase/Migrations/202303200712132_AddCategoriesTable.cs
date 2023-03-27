6d69983f1a6d619656b060a02003bdafb865a8fe
prashant06 <prajadhav06@gmail.com>
2023-03-27 18:36:03 +0530

    Add project files.

---
+++ CodeFirstFromExstingDataBase/CodeFirstFromExstingDataBase/Migrations/202303200712132_AddCategoriesTable.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeFirstFromExstingDataBase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: false),
+                    Name = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+
+            Sql("INSERT INTO Categories VALUES (1, 'Web Development')");
+            Sql("INSERT INTO Categories VALUES (2, 'Data Science')");
+            Sql("INSERT INTO Categories VALUES (3, 'NetWorking')");
+
+        }
+
+
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ FluentAPI/FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            ToTable("tab_Course");
+
+            HasKey(c => c.Id);
+
+
+            Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+
+            Property(c => c.Description)
+             .HasMaxLength(2000);
+
+
+            HasRequired(c => c.Author)
+            .WithMany(a => a.Courses)
+            .HasForeignKey(c => c.AuthorId)
+            .WillCascadeOnDelete(false);
+
+
+            HasMany(c => c.Tags)
+            .WithMany(t => t.Courses)
+            .Map(m =>
+            {
+                m.ToTable("CourseTags");
+                m.MapLeftKey("CourseId");
+                m.MapRightKey("TagId");
+            });
+
+
+            HasRequired(c => c.Cover)
+            .WithRequiredPrincipal(c => c.Course);
+
+        }
+    }
+}
